6421be68a67fe3302e176796fb196527c08bc635
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-06-18 21:22:01 +0200

    Update: FooterLayout style

--- src/components/_partials/FooterLayout.tsx
+++ src/components/_partials/FooterLayout.tsx
@@ -14,10 +14,12 @@ export default class FooterLayout extends React.Component<FooterLayoutProps> {
     return (
       <footer className={ FooterLayoutStyles.footerLayout }>
         <span className={ FooterLayoutStyles.back }>
-          <svg fill="#FFFFFF" enable-background="new 0 0 16 16" height="16" viewBox="0 0 64 64" width="16" xmlns="http://www.w3.org/2000/svg">
-            <path d="m44.586 2.586-28 28c-.781.781-.781 2.047 0 2.828l28 28 2.828-2.828-26.585-26.586 26.585-26.586z"/>
-          </svg>
-          <Link to="/">Back</Link>
+          <Link to="/">
+            <svg className={ FooterLayoutStyles.backArrow } fill="currentColor" enableBackground="new 0 0 16 16" height="16" viewBox="0 0 64 64" width="16" xmlns="http://www.w3.org/2000/svg">
+              <path d="m44.586 2.586-28 28c-.781.781-.781 2.047 0 2.828l28 28 2.828-2.828-26.585-26.586 26.585-26.586z"/>
+            </svg>
+            Back
+          </Link>
         </span>
         <span className={ FooterLayoutStyles.copyright }>
           Copyright © { new Date(now).getFullYear() } DAYMARK
