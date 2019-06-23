5c130f629d5e290fb1eff6fccd791eed823f9b02
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-06-24 00:30:25 +0200

    Edit: use graphql/json for Articles

--- src/pages/press.tsx
+++ src/pages/press.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
 
 import FooterLayout from './../components/_partials/FooterLayout'
+import { ArticleEdge } from './../types/Article'
 
 import * as PressStyles from './../styles/pages/Press.module.scss'
+
+const articles = graphql`
+  query ArticlesQuery {
+   articles: allArticlesJson {
+     edges {
+       node {
+         id
+         blockquote
+         link
+       }
+     }
+   }
+  }
+`
 
 export default class Press extends React.Component<PressProps> {
   constructor(props: PressProps) {
@@ -11,16 +27,37 @@ export default class Press extends React.Component<PressProps> {
 
   public render(): JSX.Element {
     return (
-      <>
-        <main className={ PressStyles.daymarkpress }>
-          <h1>Press</h1>
-          <div className={ PressStyles.pressList }>
-          </div>
-        </main>
-        <FooterLayout />
-      </>
+      <StaticQuery
+        query={ articles }
+        render= {
+          data => (
+            <>
+              <main className={ PressStyles.daymarkpress }>
+                <h1>Press</h1>
+                <div className={ PressStyles.pressList }>
+                  {
+                    data.articles.edges.map((articleEdge: ArticleEdge) => (
+                      <blockquote id={ articleEdge.node.id }
+                                  key={ articleEdge.node.id }>
+                                  { articleEdge.node.blockquote }
+                      </blockquote>
+                    ))
+                  }
+                </div>
+              </main>
+              <FooterLayout />
+            </>
+          )
+        }/>
     )
   }
 }
 
-interface PressProps {}
+
+interface PressProps {
+  data: {
+    articles: {
+      edges: ArticleEdge[]
+    }
+  }
+}
