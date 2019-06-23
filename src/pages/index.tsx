8ce8f43cd18a64703010d84e02cfbcf1d878d304
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-06-23 23:53:35 +0200

    Edit: use external Favicon component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import * as styles from './Index.module.scss'
 import Home from './../components/Home/Home'
 import Menu from './../components/_partials/Menu'
 import Footer from './../components/_partials/Footer'
+import { Favicon } from './../components/_partials/Favicon'
 
 interface IndexPageProps {
   data: {
@@ -46,23 +47,7 @@ export default class IndexPage extends React.Component<IndexPageProps, {}> {
 
           <meta name="twitter:description" content="" />
           <meta name="twitter:image:src" content="https://thedaymark.com" />
-          <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicon/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicon/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicon/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicon/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192"  href="assets/images/favicon/android-icon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="assets/images/favicon/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="assets/images/favicon/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#ffffff" />
+          { Favicon }
         </Helmet>
         <Menu />
         <Home />
