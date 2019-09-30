4b3553503089a0466ceb54021dcce3b3af8df439
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-10-01 01:54:34 +0200

    Add: Music platforms

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
 
 import { Head } from './../components/_partials/Head'
 import FooterLayout from './../components/_partials/FooterLayout'
+import { PlatformEdge } from './../types/Platform'
 
 import * as MusicStyles from './../styles/pages/Music.module.scss'
+
+const musicPlatforms = graphql`
+  query PlatformsQuery {
+    platforms: allPlatformsJson {
+      edges {
+        node {
+          id
+          link
+          imageURL
+        }
+      }
+    }
+  }
+`
 
 export default class Music extends React.Component<MusicProps> {
   constructor(props: MusicProps) {
@@ -12,13 +28,30 @@ export default class Music extends React.Component<MusicProps> {
 
   public render(): JSX.Element {
     return (
-      <>
-        <Head title="Music"/>
-        <main className={ MusicStyles.daymarkmusic }>
-          <h1 className={ MusicStyles.musicList }>Music</h1>
-        </main>
-        <FooterLayout />
-      </>
+      <StaticQuery
+        query={ musicPlatforms }
+        render= {
+          data => (
+            <>
+              <Head title="Music"/>
+              <main className={ MusicStyles.daymarkmusic }>
+                <h1>Music</h1>
+                <div className={ MusicStyles.musicList }>
+                  {
+                    data.platforms.edges.map((articleEdge: PlatformEdge) => (
+                      <div className={ MusicStyles.musicItem }>
+                        <a href={ articleEdge.node.link } target="_blank">
+                          <img src={ articleEdge.node.imageURL } alt={ articleEdge.node.id }/>
+                        </a>
+                      </div>
+                    ))
+                  }
+                </div>
+              </main>
+              <FooterLayout />
+            </>
+          )
+        }/>
     )
   }
 }
---
+++ src/types/Platform.ts
@@ -0,0 +1,9 @@
+export type Platform = {
+  id: string
+  link: string
+  imageURL: string
+}
+
+export type PlatformEdge = {
+  node: Platform
+}
