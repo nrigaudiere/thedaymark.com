edd6bc01df7fd0b6b7d506161907e303135f0808
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-10-06 12:46:22 +0200

    Add: meta Og

--- src/helpers/Meta.helper.ts
+++ src/helpers/Meta.helper.ts
@@ -1,6 +1,7 @@
 class Meta {
   public readonly SITE_NAME: string = 'DAYMARK'
   public readonly SITE_URL: string = 'https://thedaymark.com'
+  public readonly OG_IMG: string = '/images/og-tg.jpg'
 }
 
 const MetaHelper = new Meta()
