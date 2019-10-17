dd48f4551869f4916d0f60f05eed32a6e66affbf
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-10-17 13:11:45 +0200

    Fix: style for press

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -18,8 +18,8 @@ export default class Videos extends React.Component<VideosProps> {
         <main className={ VideoStyles.daymarkvideo }>
           <h1>Videos</h1>
           <div className={ VideoStyles.videoList }>
-            <ReactPlayer className={ VideoStyles.videoElement } url="https://www.youtube.com/watch?v=l04bv5F6bJI" controls width="45%" height="auto"/>
             <ReactPlayer className={ VideoStyles.videoElement } url="https://www.youtube.com/watch?v=FisQ3uGdmJY" controls width="45%" height="auto"/>
+            <ReactPlayer className={ VideoStyles.videoElement } url="https://www.youtube.com/watch?v=l04bv5F6bJI" controls width="45%" height="auto"/>
             <ReactPlayer className={ VideoStyles.videoElement } url="https://www.youtube.com/watch?v=yF1IwcBd5kU" controls width="45%" height="auto"/>
           </div>
         </main>
