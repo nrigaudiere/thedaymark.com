639695f3897f591c78b7d39be5c1e090ddb1a07e
Nicolas Rigaudiere <i.am.a@curious.engineer>
2019-10-17 20:05:10 +0200

    Update: bio & meta

--- src/components/_partials/Head.tsx
+++ src/components/_partials/Head.tsx
@@ -6,19 +6,19 @@ import { Favicon } from './Favicon'
 import { MetaHelper } from '@daymark/helpers/Meta.helper'
 
 export function Head(props: HeadProps): JSX.Element {
-  const title = props.title ? `${ props.title } | ${ MetaHelper.SITE_NAME }` : `${ MetaHelper.SITE_NAME } | Electro-Rock`
+  const title = props.title ? `${ props.title } | ${ MetaHelper.SITE_NAME }` : `${ MetaHelper.SITE_NAME } | Post-Pop`
 
   return (
     <Helmet title={ title } defer={false}>
       <meta property="og:title" content={ title } />
 
-      <meta property="og:description" content="" />
-      <meta property="og:image" content={ MetaHelper.OG_IMG } />
+      <meta property="og:description" content={ MetaHelper.SITE_CONTENT } />
+      <meta property="og:image" content={ MetaHelper.SITE_URL + MetaHelper.OG_IMG } />
       <meta property="og:url" content={ MetaHelper.SITE_URL } />
 
       <meta name="twitter:title" content={ title } />
-      <meta name="twitter:description" content="" />
-      <meta name="twitter:image:src" content={ MetaHelper.OG_IMG } />
+      <meta name="twitter:description" content={ MetaHelper.SITE_CONTENT } />
+      <meta name="twitter:image:src" content={ MetaHelper.SITE_URL + MetaHelper.OG_IMG } />
       { Favicon }
     </Helmet>
   )
